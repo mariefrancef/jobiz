e898c464411580d2c19f617818dd1cd5a35abaec
Lucas Plebani <lucas.plebani@outlook.fr>
2025-06-05 15:43:27 +0200

    add about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,7 @@ module.exports = {
   content: ["./assets/**/*.js", "./templates/**/*.html.twig"],
   theme: {
     extend: {
-      colors: {
-        primary: colors.slate[700], // => #334155
-        accent: colors.teal[600], // => #0d9488
-      },
+      
       fontFamily: {
         sans: ["system-ui", "sans-serif"],
       },
